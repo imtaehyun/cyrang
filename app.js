2b3ca1a8659fce57ee1a43f1a59823a2303de924
EC2 Default User <ec2-user@ip-10-152-95-28.ap-northeast-1.compute.internal>
2012-11-29 09:06:04 +0000

    add json parse logic & nodemon module

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 8080);
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
@@ -26,6 +26,52 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
+
+
+
+function cyrang_interest(seq, item_count, item) {
+    this.seq = seq;
+    this.item_count = item_count;
+    this.items = items;
+}
+
+var option = {
+    host: 'cyrang.cyworld.com',
+    path: '/if/get_interest',
+    method: 'GET'
+}
+
+var loop = function(url) {
+    var request = http.request(url, function(res) {
+        var body ='';
+        res.on('data', function(chunk) {
+            body += chunk.toString('utf8');
+        });
+        res.on('end', function(chunk) {
+            var jsonObj = JSON.parse(body);
+
+            var seq = jsonObj.result.set[0].seq;
+            var item_count = jsonObj.result.set[0].item_count;
+            var items = jsonObj.result.set[0].item;
+
+            console.log("next_seq : " + seq);
+            console.log("item_count : " + item_count);
+//        console.log("item : " + JSON.stringify(items));
+
+            //http://cyrang.cyworld.com/if/get_interest?tocy_info=1&last_seq=281&t=1354173399340
+            option = {
+                host : 'cyrang.cyworld.com',
+                path : '/if/get_interest?tocy_info=1&last_seq=' + seq,
+                method : 'GET'
+            }
+            console.log(option);
+        });
+    });
+    request.end();
+};
+setInterval(function(){
+    loop(option);
+}, 15000);
 
 app.get('/', routes.index);
 app.get('/users', user.list);
