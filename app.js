9f57456afb2d99c513095a3b6a3c2d24afdc5885
EC2 Default User <ec2-user@ip-10-152-95-28.ap-northeast-1.compute.internal>
2012-12-04 08:40:42 +0000

    optimize app.js & add more button & change module from nodemon to forever

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 80);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -25,14 +25,20 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-var option = {
-    host: 'cyrang.cyworld.com',
-    path: '/if/get_interest',
-    method: 'GET'
-};
-
-app.get('/', function(request, response) {
-    var request = http.request(option, function(res) {
+function getInterestOption(seq) {
+    var option = {
+        host: 'cyrang.cyworld.com',
+        path: '/if/get_interest',
+        method: 'GET'
+    };
+    if (seq != null && seq != '') {
+        option.path = '/if/get_interest?tocy_info=1&last_seq=' + seq;
+    }
+    console.log(option);
+    return option;
+}
+function request(option, fn) {
+    http.request(option, function(res) {
         var body ='';
         res.on('data', function(chunk) {
             body += chunk.toString('utf8');
@@ -40,28 +46,38 @@ app.get('/', function(request, response) {
         res.on('end', function(chunk) {
             var jsonObj = JSON.parse(body);
 
-            var seq = jsonObj.result.set[0].seq;
-            var item_count = jsonObj.result.set[0].item_count;
+            var nextSeq = jsonObj.result.set[0].seq;
+            var itemCount = jsonObj.result.set[0].item_count;
             var items = jsonObj.result.set[0].item;
 
-            console.log("next_seq : " + seq);
-            console.log("item_count : " + item_count);
+            console.log("next_seq : " + nextSeq);
+            console.log("item_count : " + itemCount);
 
-            option = {
-                host : 'cyrang.cyworld.com',
-                path : '/if/get_interest?tocy_info=1&last_seq=' + seq,
-                method : 'GET'
-            };
-            response.render('interest', {list : items});
+            return fn(nextSeq, items);
         });
-
         res.on('error', function(e) {
             console.log('Problem with request: ' + e.message);
         });
-    });
-    request.end();
-});
+    }).end();
+}
 
+app.get('/', function(req, res) {
+    console.log(req.header('host'));
+    res.redirect('/interest');
+});
+app.get('/interest', function(req, res) {
+    console.log('Normal Access');
+    request(getInterestOption(null), function(nextSeq, items) {
+        res.render('interest', {title: "Cyrang Interest", list : items, seq: nextSeq});
+    });
+});
+app.get('/interest/:seq', function(req, res) {
+    var seq = req.params.seq;
+    console.log('Requested seq: ' + seq);
+    request(getInterestOption(seq), function(nextSeq, items) {
+        res.render('interest', {title: "Cyrang Interest", list : items, seq: nextSeq});
+    });
+});
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
